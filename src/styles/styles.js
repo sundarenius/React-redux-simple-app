4208b83fb7f4ff36c01121d489a9be9f216814f4
Håkan Sundström <Sundarenius@Hakans-MBP-2.lan>
2019-12-10 23:28:53 +0100

    commit

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -2,10 +2,12 @@ import index from './index.scss'
 import footer from './footer.scss'
 import header from './header.scss'
 import mainPage from './main-page.scss'
+import App from './App.scss'
 
 export default `
 ${index}
 ${footer}
 ${header}
 ${mainPage}
+${App}
 `
